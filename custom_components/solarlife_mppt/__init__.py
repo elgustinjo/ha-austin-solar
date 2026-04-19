b5c6f65d900e2e8877e113d9d655ada53863ef75
elgustinjo <elgustinjo@users.noreply.github.com>
2026-04-19 16:12:47 +0200

    Update __init__.py

--- custom_components/solarlife_mppt/__init__.py
+++ custom_components/solarlife_mppt/__init__.py
@@ -26,9 +26,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-await hass.async_create_task(
-    hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-)
     return True
 
 
